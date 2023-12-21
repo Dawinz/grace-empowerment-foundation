9959eb64609b82d7676a1a279c74eab803f9d16f
Dawinz <71150010+Dawinz@users.noreply.github.com>
2023-12-21 08:24:32 +0300

    some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,6 @@ function NavbarComponent() {
           <Nav className="mx-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/projects">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="Contact">Contact</Nav.Link>
             {/* Add more Nav.Link components as needed */}
